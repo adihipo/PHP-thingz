9256aff7f449568b6f4efd4280d331c35f4d319c
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-03-02 16:36:23 +0100

    feat(codeigntier app): first custom view as hello_world.php

--- CodeIgniter_app/application/controllers/Hello.php
+++ CodeIgniter_app/application/controllers/Hello.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Hello extends CI_Controller {
 
 	public function index() {
-		echo "Hello World!";
+		$this->load->view("hello_world");
 	}
 }
 
